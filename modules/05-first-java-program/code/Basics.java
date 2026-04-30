df48a8b9e0c1c63f44d8f3411e9bccc1effdea76
‘Kalophain14’ <chueutemosho@gmail.com>
2026-05-01 00:10:00 +0200

    added some files on the git ignore

--- modules/05-first-java-program/code/Basics.java
+++ modules/05-first-java-program/code/Basics.java
@@ -3,7 +3,24 @@ package kalo.first_java_program;
 public class Basics {
 
     public static void main(String[] args) {
-        System.out.println("awesshsh");
+        System.out.println("Hello, Kalo!");
+
+        // basic math
+        int a = 5;
+        int b = 10;
+        System.out.println("Sum: " + (a + b));
+        System.out.println("Product: " + (a * b));
+
+        // string
+        String name = "Kalophain";
+        System.out.println("Name: " + name);
+        System.out.println("Name length: " + name.length());
+
+        // loop
+        System.out.println("Counting to 5:");
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(i);
+        }
     }
 
 }
