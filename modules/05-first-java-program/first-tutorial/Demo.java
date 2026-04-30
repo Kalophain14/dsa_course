65a189abf7919b2b59e6027ab827fd5b78314353
‘Kalophain14’ <chueutemosho@gmail.com>
2026-05-01 00:03:56 +0200

    refactor: remove duplicate first-tutorial subfolder

--- modules/05-first-java-program/first-tutorial/Demo.java
+++
@@ -1,9 +0,0 @@
-package kalo.first_java_program;
-
-public class Demo {
-
-    public static void main(String[] args) {
-
-    }
-
-}
--- modules/05-first-java-program/first-tutorial/Main.java
+++
@@ -1,9 +0,0 @@
-package kalo.first_java_program;
-
-public class Main {
-
-    public static void main(String[] args) {
-
-    }
-
-}
