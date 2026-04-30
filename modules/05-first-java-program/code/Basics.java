d4c31a09f78d48ae8832ea458f9599a7dbea8b2c
‘Kalophain14’ <chueutemosho@gmail.com>
2026-05-01 00:06:49 +0200

    modified

--- modules/05-first-java-program/code/Basics.java
+++ modules/05-first-java-program/code/Basics.java
@@ -3,7 +3,7 @@ package kalo.first_java_program;
 public class Basics {
 
     public static void main(String[] args) {
-        System.out.println("hi");
+        System.out.println("awesshsh");
     }
 
 }
